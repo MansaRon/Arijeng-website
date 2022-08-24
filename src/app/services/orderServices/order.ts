3485eb0d5a5517a99e0adb0ead7a4503785044fd
Thendo Ramashia <kramashia101@gmail.com>
2022-08-24 11:07:56 +0200

    Endpoints added on admin side.

--- src/app/services/orderServices/order.ts
+++ src/app/services/orderServices/order.ts
@@ -1,7 +1,30 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { OrderUpdate } from "src/app/models/order-update";
 import { Endpoints } from "../urlServices/endpoints";
 
-@Injectable()
+@Injectable({providedIn: 'any'})
 export class Order {
-    constructor(private endpoints: Endpoints) {}
+    constructor(private endpoints: Endpoints, private http: HttpClient) {}
+
+    public getAllOrders(): Observable<any> {
+        return this.http.get(this.endpoints.arijengurl() + "order/orders", {
+            headers: new HttpHeaders()
+                    .set("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token, content-type")
+                    .set("Access-Control-Allow-Origin", "*")
+                    .set("Accept", 'application/json')
+                    .set('Content-Type', 'application/json')
+        }) 
+    }
+
+    public updateOrderStatus(orderId: String, updateOrderDTO: OrderUpdate): Observable<any> {
+        return this.http.put(this.endpoints.arijengurl() + "order/" + orderId + "/update", updateOrderDTO, {
+            headers: new HttpHeaders()
+                    .set("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token, content-type")
+                    .set("Access-Control-Allow-Origin", "*")
+                    .set("Accept", 'application/json')
+                    .set('Content-Type', 'application/json')
+        }) 
+    }
 }
