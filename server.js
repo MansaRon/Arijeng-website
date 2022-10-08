c0636514f248d65cca9e02e829b0d1bdcf315d1d
Thendo Ramashia <kramashia101@gmail.com>
2022-10-08 22:54:06 +0200

    Fixed the name of the app.

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/arijeng'));
+app.use(express.static(__dirname + '/dist/order-app'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname+'/dist/arijeng/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/order-app/index.html'));
 });
 
 // Start the app by listening on the port provided by the environment
